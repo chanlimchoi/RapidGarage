c110521122bf53db7daaf4d7c05428363680a664
Ahmed Abdelrahman <ahmed_3m2010@hotmail.com>
2017-11-08 21:11:41 -0500

    Create the users and posts tables for the database

---
+++ rapidgarage-backend/models/post.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Post = sequelize.define('Post', {
+    street: DataTypes.STRING,
+    city: DataTypes.STRING,
+    zipCode: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
+    imagesUrl: DataTypes.STRING,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Post.belongsTo(models.User);
+      }
+    }
+  });
+  return Post;
+};
