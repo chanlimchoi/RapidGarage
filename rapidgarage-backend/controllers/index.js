dce213de3936fd8f185a5d6f4368c4bb6bedb75c
Ahmed Abdelrahman <ahmed_3m2010@hotmail.com>
2017-11-16 20:21:47 -0500

    Add authentication code

--- rapidgarage-backend/controllers/index.js
+++ rapidgarage-backend/controllers/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.use('/login', require('./login'));
+router.use('/signup', require('./signup'));
 router.use('/', require('./home'));
-
 
 module.exports = router;
