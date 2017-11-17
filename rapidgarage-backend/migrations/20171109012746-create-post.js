119783e6296d0cb3dd0dc8480cd598701fed4399
Ahmed Abdelrahman <ahmed_3m2010@hotmail.com>
2017-11-16 20:26:08 -0500

    Fix bug with datatype in Post model

--- rapidgarage-backend/migrations/20171109012746-create-post.js
+++ rapidgarage-backend/migrations/20171109012746-create-post.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       title: {
-        type: Sequelize.String
+        type: Sequelize.STRING
       },
       description: {
         type: Sequelize.TEXT
